9d03e50831e2e97264dcc58b3b2d07be71b0f21e
Sergiy Sych <sychsergiy@gmail.com>
2020-05-18 05:46:15 +0300

    add purchase model, registore to admin tool (#2)

--- apps/purchase/models.py
+++ apps/purchase/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import enum
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from apps.asset.models import Asset
+
+
+class Purchase(models.Model):
+    class Types(enum.Enum):
+        BUY = "Buy"
+        SELL = "Sell"
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    asset = models.ForeignKey(Asset, on_delete=models.PROTECT)
+    asset_amount = models.FloatField()
+    absolute_amount = models.FloatField()
+    type = models.CharField(
+        max_length=68, choices=[(tag.name, tag.value) for tag in Types]
+    )
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} {self.asset}"
